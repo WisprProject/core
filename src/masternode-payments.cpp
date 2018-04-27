627772c4b271336de8707340701660baf5a4d211
MastaZz <Dennisvdongen87@gmail.com>
2018-04-27 21:26:06 +0200

    Testnet Adjustments
    Testnet Adjustments

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2014-2015 The Dash developers
-// Copyright (c) 2015-2017 The Wispr developers
+// Copyright (c) 2015-2018 The Wispr developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -515,7 +515,7 @@ bool CMasternodeBlockPayees::IsTransactionValid(const CTransaction& txNew)
 
     int nMaxSignatures = 0;
     int nMasternode_Drift_Count = 0;
-    
+
     std::string strPayeesPossible = "";
 
     CAmount nReward = GetBlockValue(nBlockHeight);
