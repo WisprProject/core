8ef28650480bda44262f66f6b83f8d1710bda62f
Thijs <thetcr@live.nl>
2018-05-08 01:15:37 +0200

    add scrypt functions

--- src/crypto/scrypt.h
+++ src/crypto/scrypt.h
@@ -5,5 +5,9 @@
 #include <string>
 
 void scrypt(const char* pass, unsigned int pLen, const char* salt, unsigned int sLen, char *output, unsigned int N, unsigned int r, unsigned int p, unsigned int dkLen);
+uint256 scrypt_salted_multiround_hash(const void* input, size_t inputlen, const void* salt, size_t saltlen, const unsigned int nRounds);
+uint256 scrypt_salted_hash(const void* input, size_t inputlen, const void* salt, size_t saltlen);
+uint256 scrypt_hash(const void* input, size_t inputlen);
+uint256 scrypt_blockhash(const void* input);
 
 #endif
