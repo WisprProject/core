63df4303a8e7d5f42d32b771216d09b6bb54e2a9
Thijs <thetcr@live.nl>
2018-05-07 23:53:10 +0200

    replace crypto files with scrypt

--- src/crypto/scrypt.h
+++ src/crypto/scrypt.h
@@ -1,9 +1,15 @@
-#ifndef SCRYPT_H
-#define SCRYPT_H
-#include <stdlib.h>
+#ifndef SCRYPT_MINE_H
+#define SCRYPT_MINE_H
+
 #include <stdint.h>
-#include <string>
+#include <stdlib.h>
 
-void scrypt(const char* pass, unsigned int pLen, const char* salt, unsigned int sLen, char *output, unsigned int N, unsigned int r, unsigned int p, unsigned int dkLen);
+#include "util.h"
+#include "net.h"
 
-#endif
+uint256 scrypt_salted_multiround_hash(const void* input, size_t inputlen, const void* salt, size_t saltlen, const unsigned int nRounds);
+uint256 scrypt_salted_hash(const void* input, size_t inputlen, const void* salt, size_t saltlen);
+uint256 scrypt_hash(const void* input, size_t inputlen);
+uint256 scrypt_blockhash(const void* input);
+
+#endif // SCRYPT_MINE_H
