048aa5cd6f338bc21c3fd7d202a469d40c07b018
Thijs <thetcr@live.nl>
2018-05-08 03:05:01 +0200

    add scryptHash

--- src/crypter.cpp
+++ src/crypter.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "crypter.h"
-
+#include "scrypt.h"
 #include "script/script.h"
 #include "script/standard.h"
 #include "util.h"
