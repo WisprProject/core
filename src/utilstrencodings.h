bede46c52a3f4143b7d974cd2e12d86d206bfa5e
Thijs <thetcr@live.nl>
2018-05-10 22:17:38 +0200

    Update utilstr

--- src/utilstrencodings.h
+++ src/utilstrencodings.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin developers
+// Copyright (c) 2017 The PIVX developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -71,7 +72,7 @@ std::string HexStr(const T itbegin, const T itend, bool fSpaces = false)
 {
     std::string rv;
     static const char hexmap[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
-        '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+                                    '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
     rv.reserve((itend - itbegin) * 3);
     for (T it = itbegin; it < itend; ++it) {
         unsigned char val = (unsigned char)(*it);
@@ -102,7 +103,7 @@ inline std::string ReverseEndianString(std::string in)
     return out;
 }
 
-/** 
+/**
  * Format a paragraph of text to a fixed width, adding spaces for
  * indentation to any added line.
  */
