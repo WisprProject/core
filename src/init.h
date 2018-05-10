ee52fe5c3d18459062772029e7c585a979b559df
Thijs <thetcr@live.nl>
2018-05-10 20:11:33 +0200

    Update init

--- src/init.h
+++ src/init.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin developers
+// Copyright (c) 2017 The PIVX developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -8,20 +9,25 @@
 
 #include <string>
 
+class CScheduler;
 class CWallet;
+class CzPIVWallet;
 
 namespace boost
 {
-class thread_group;
+    class thread_group;
 } // namespace boost
 
 extern CWallet* pwalletMain;
+extern CzPIVWallet* zwalletMain;
 
 void StartShutdown();
 bool ShutdownRequested();
+/** Interrupt threads */
+void Interrupt(boost::thread_group& threadGroup);
 void Shutdown();
 void PrepareShutdown();
-bool AppInit2(boost::thread_group& threadGroup);
+bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler);
 
 /** The help message mode determines what help message to show */
 enum HelpMessageMode {
