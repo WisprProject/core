e145e8ed130117e232ed69b453cc101a2b9a858e
Thijs <thetcr@live.nl>
2018-05-10 21:30:33 +0200

    update ui interface

--- src/ui_interface.h
+++ src/ui_interface.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2010 Satoshi Nakamoto
-// Copyright (c) 2012 The Bitcoin developers
+// Copyright (c) 2012-2014 The Bitcoin developers
+// Copyright (c) 2017-2018 The PIVX developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -36,10 +37,10 @@ public:
          * Mask of all available icons in CClientUIInterface::MessageBoxFlags
          * This needs to be updated, when icons are changed there!
          */
-        ICON_MASK = (ICON_INFORMATION | ICON_WARNING | ICON_ERROR),
+                ICON_MASK = (ICON_INFORMATION | ICON_WARNING | ICON_ERROR),
 
         /** These values are taken from qmessagebox.h "enum StandardButton" to be directly usable */
-        BTN_OK = 0x00000400U,      // QMessageBox::Ok
+                BTN_OK = 0x00000400U,      // QMessageBox::Ok
         BTN_YES = 0x00004000U,     // QMessageBox::Yes
         BTN_NO = 0x00010000U,      // QMessageBox::No
         BTN_ABORT = 0x00040000U,   // QMessageBox::Abort
@@ -55,22 +56,22 @@ public:
          * Mask of all available buttons in CClientUIInterface::MessageBoxFlags
          * This needs to be updated, when buttons are changed there!
          */
-        BTN_MASK = (BTN_OK | BTN_YES | BTN_NO | BTN_ABORT | BTN_RETRY | BTN_IGNORE |
-                    BTN_CLOSE |
-                    BTN_CANCEL |
-                    BTN_DISCARD |
-                    BTN_HELP |
-                    BTN_APPLY |
-                    BTN_RESET),
+                BTN_MASK = (BTN_OK | BTN_YES | BTN_NO | BTN_ABORT | BTN_RETRY | BTN_IGNORE |
+                            BTN_CLOSE |
+                            BTN_CANCEL |
+                            BTN_DISCARD |
+                            BTN_HELP |
+                            BTN_APPLY |
+                            BTN_RESET),
 
         /** Force blocking, modal message box dialog (not just OS notification) */
-        MODAL = 0x10000000U,
+                MODAL = 0x10000000U,
 
         /** Do not print contents of message to debug log */
-        SECURE = 0x40000000U,
+                SECURE = 0x40000000U,
 
         /** Predefined combinations for certain default usage cases */
-        MSG_INFORMATION = ICON_INFORMATION,
+                MSG_INFORMATION = ICON_INFORMATION,
         MSG_WARNING = (ICON_WARNING | BTN_OK | MODAL),
         MSG_ERROR = (ICON_ERROR | BTN_OK | MODAL)
     };
@@ -101,6 +102,12 @@ public:
 
     /** New block has been accepted */
     boost::signals2::signal<void(const uint256& hash)> NotifyBlockTip;
+
+    /** New block has been accepted and is over a certain size */
+    boost::signals2::signal<void(int size, const uint256& hash)> NotifyBlockSize;
+
+    /** Banlist did change. */
+    boost::signals2::signal<void (void)> BannedListChanged;
 };
 
 extern CClientUIInterface uiInterface;
