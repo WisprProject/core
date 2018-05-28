fdc80295d211c415bb1635b08004c9e712d8ce41
Thijs <thetcr@live.nl>
2018-05-28 18:04:50 +0200

    Implement old CHashWriter class.

--- src/hash.h
+++ src/hash.h
@@ -231,36 +231,36 @@ inline uint160 Hash160(const std::vector<unsigned char>& vch)
 class CHashWriter
 {
 private:
-    CHash256 ctx;
+    SHA256_CTX ctx;
 
 public:
     int nType;
     int nVersion;
 
-    CHashWriter(int nTypeIn, int nVersionIn) : nType(nTypeIn), nVersion(nVersionIn) {}
+    void Init() {
+        SHA256_Init(&ctx);
+    }
 
-    CHashWriter& write(const char* pch, size_t size)
-    {
-        ctx.Write((const unsigned char*)pch, size);
+    CHashWriter(int nTypeIn, int nVersionIn) : nType(nTypeIn), nVersion(nVersionIn) {
+        Init();
+    }
+
+    CHashWriter& write(const char *pch, size_t size) {
+        SHA256_Update(&ctx, pch, size);
         return (*this);
     }
 
     // invalidates the object
-    uint256 GetHash()
-    {
+    uint256 GetHash() {
         uint256 hash1;
         SHA256_Final((unsigned char*)&hash1, &ctx);
         uint256 hash2;
         SHA256((unsigned char*)&hash1, sizeof(hash1), (unsigned char*)&hash2);
         return hash2;
-//        uint256 result;
-//        ctx.Finalize((unsigned char*)&result);
-//        return result;
     }
 
-    template <typename T>
-    CHashWriter& operator<<(const T& obj)
-    {
+    template<typename T>
+    CHashWriter& operator<<(const T& obj) {
         // Serialize to this stream
         ::Serialize(*this, obj, nType, nVersion);
         return (*this);
