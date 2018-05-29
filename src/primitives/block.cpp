55da1d61b7aa0050c65b7046ac265220685a114e
Thijs <thetcr@live.nl>
2018-05-30 00:18:14 +0200

    Remove logging of AccumulatorCheckpoint

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -123,12 +123,12 @@ uint256 CBlock::CheckMerkleBranch(uint256 hash, const std::vector<uint256>& vMer
 std::string CBlock::ToString() const
 {
     std::stringstream s;
-    s << strprintf("CBlock(hash=%s, ver=%d, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, nAccumulatorCheckpoint=%s, vtx=%u)\n",
+    s << strprintf("CBlock(hash=%s, ver=%d, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%u)\n",
                    GetHash().ToString(),
                    nVersion,
                    hashPrevBlock.ToString(),
                    hashMerkleRoot.ToString(),
-                   nTime, nBits, nNonce, nAccumulatorCheckpoint,
+                   nTime, nBits, nNonce,
                    vtx.size());
     for (unsigned int i = 0; i < vtx.size(); i++)
     {
