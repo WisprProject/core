ade9616f41089ae15235163f4b4ee443c2fea98f
Thijs <thetcr@live.nl>
2018-06-05 16:50:08 +0200

    We don't have a fork.

--- src/main.cpp
+++ src/main.cpp
@@ -4141,15 +4141,15 @@ bool CheckWork(const CBlock block, CBlockIndex* const pindexPrev)
 
     unsigned int nBitsRequired = GetNextWorkRequired(pindexPrev, &block);
 
-    if (block.IsProofOfWork() && (pindexPrev->nHeight + 1 <= 68589)) {
-        double n1 = ConvertBitsToDouble(block.nBits);
-        double n2 = ConvertBitsToDouble(nBitsRequired);
-
-        if (abs(n1 - n2) > n1 * 0.5)
-            return error("%s : incorrect proof of work (DGW pre-fork) - %f %f %f at %d", __func__, abs(n1 - n2), n1, n2, pindexPrev->nHeight + 1);
-
-        return true;
-    }
+//    if (block.IsProofOfWork() && (pindexPrev->nHeight + 1 <= 68589)) {
+//        double n1 = ConvertBitsToDouble(block.nBits);
+//        double n2 = ConvertBitsToDouble(nBitsRequired);
+//
+//        if (abs(n1 - n2) > n1 * 0.5)
+//            return error("%s : incorrect proof of work (DGW pre-fork) - %f %f %f at %d", __func__, abs(n1 - n2), n1, n2, pindexPrev->nHeight + 1);
+//
+//        return true;
+//    }
 
     if (block.nBits != nBitsRequired)
         return error("%s : incorrect proof of work at %d", __func__, pindexPrev->nHeight + 1);
