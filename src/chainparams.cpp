c8b3a4a29bd57b46295505a3a26021f2688f45ca
Thijs <thetcr@live.nl>
2018-06-07 13:47:08 +0200

    Skip hash check

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -203,7 +203,7 @@ public:
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;
         fMineBlocksOnDemand = false;
-        fSkipProofOfWorkCheck = false;
+        fSkipProofOfWorkCheck = true;
         fTestnetToBeDeprecatedFieldRPC = false;
         fHeadersFirstSyncingActive = false;
 
