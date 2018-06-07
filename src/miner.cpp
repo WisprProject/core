75f7c22a84b96eb0d3b750f949091621f829151e
Thijs <thetcr@live.nl>
2018-06-07 13:58:12 +0200

    Log miner.

--- src/miner.cpp
+++ src/miner.cpp
@@ -458,8 +458,10 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
         uint256 hashBlockLastAccumulated = chainActive[nHeight - (nHeight % 10) - 10]->GetBlockHash();
         if (nHeight >= pCheckpointCache.first || pCheckpointCache.second.first != hashBlockLastAccumulated) {
             //For the period before v2 activation, zWSP will be disabled and previous block's checkpoint is all that will be needed
+            printf("CreateNewBlock(): Check for zerocoin V2 start\n");
             pCheckpointCache.second.second = pindexPrev->nAccumulatorCheckpoint;
             if (pindexPrev->nHeight + 1 >= Params().Zerocoin_Block_V2_Start()) {
+                printf("CreateNewBlock(): Map accumulators\n");
                 AccumulatorMap mapAccumulators(Params().Zerocoin_Params(false));
                 if (fZerocoinActive && !CalculateAccumulatorCheckpoint(nHeight, nCheckpoint, mapAccumulators)) {
                     LogPrintf("%s: failed to get accumulator checkpoint\n", __func__);
@@ -468,6 +470,7 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
                     pCheckpointCache.first = nHeight + (10 - (nHeight % 10));
 
                     // the block hash of the last block used in the accumulator checkpoint calc. This will handle reorg situations.
+                    printf("CreateNewBlock(): Use block hash of the last block for the checkpoint\n");
                     pCheckpointCache.second.first = hashBlockLastAccumulated;
                     pCheckpointCache.second.second = nCheckpoint;
                 }
