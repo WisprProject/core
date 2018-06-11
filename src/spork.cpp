3a33023fe288dc631ddcf51ef0d7d53b8f43d362
Thijs <thetcr@live.nl>
2018-06-11 16:06:32 +0200

    Remove old spork key and methods.

--- src/spork.cpp
+++ src/spork.cpp
@@ -85,19 +85,19 @@ void ProcessSpork(CNode* pfrom, std::string& strCommand, CDataStream& vRecv)
 
         LogPrintf("%s : new %s ID %d Time %d bestHeight %d\n", __func__, hash.ToString(), spork.nSporkID, spork.nValue, chainActive.Tip()->nHeight);
 
-        if (spork.nTimeSigned >= Params().NewSporkStart()) {
+//        if (spork.nTimeSigned >= Params().NewSporkStart()) {
             if (!sporkManager.CheckSignature(spork, true)) {
                 LogPrintf("%s : Invalid Signature\n", __func__);
                 Misbehaving(pfrom->GetId(), 100);
                 return;
             }
-        }
+//        }
 
-        if (!sporkManager.CheckSignature(spork)) {
-            LogPrintf("%s : Invalid Signature\n", __func__);
-            Misbehaving(pfrom->GetId(), 100);
-            return;
-        }
+//        if (!sporkManager.CheckSignature(spork)) {
+//            LogPrintf("%s : Invalid Signature\n", __func__);
+//            Misbehaving(pfrom->GetId(), 100);
+//            return;
+//        }
 
         mapSporks[hash] = spork;
         mapSporksActive[spork.nSporkID] = spork;
@@ -196,10 +196,10 @@ bool CSporkManager::CheckSignature(CSporkMessage& spork, bool fCheckSigner)
         return false;
 
     // See if window is open that allows for old spork key to sign messages
-    if (!fValidWithNewKey && GetAdjustedTime() < Params().RejectOldSporkKey()) {
-        CPubKey pubkeyold(ParseHex(Params().SporkKeyOld()));
-        return obfuScationSigner.VerifyMessage(pubkeyold, spork.vchSig, strMessage, errorMessage);
-    }
+//    if (!fValidWithNewKey && GetAdjustedTime() < Params().RejectOldSporkKey()) {
+//        CPubKey pubkeyold(ParseHex(Params().SporkKeyOld()));
+//        return obfuScationSigner.VerifyMessage(pubkeyold, spork.vchSig, strMessage, errorMessage);
+//    }
 
     return fValidWithNewKey;
 }
