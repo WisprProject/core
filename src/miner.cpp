e355852fdc22cf0bd246387767d23532949ebb3f
Thijs <thetcr@live.nl>
2018-06-14 10:06:26 +0200

    Logging.

--- src/miner.cpp
+++ src/miner.cpp
@@ -625,7 +625,7 @@ void BitcoinMiner(CWallet* pwallet, bool fProofOfStake)
                 if (!fGenerateBitcoins && !fProofOfStake)
                     continue;
             }
-
+            printf("Staking block created\n");
             if (mapHashedBlocks.count(chainActive.Tip()->nHeight)) //search our map of hashed blocks, see if bestblock has been hashed yet
             {
                 if (GetTime() - mapHashedBlocks[chainActive.Tip()->nHeight] < max(pwallet->nHashInterval, (unsigned int)1)) // wait half of the nHashDrift with max wait of 3 minutes
