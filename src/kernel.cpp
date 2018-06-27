46bb0be8c57d620771b9a1ced3071158b5609eef
Thijs <thetcr@live.nl>
2018-06-28 01:09:21 +0200

    Update hashproof for the stake modifier.

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -316,7 +316,7 @@ bool CheckStake(const uint256& bnStakeModifierV2, const CTransaction& txPrev, co
 {
     CDataStream ss(SER_GETHASH, 0);
     ss << bnStakeModifierV2;
-    ss << txPrev.nTime << prevout.hash << prevout.n << nTimeTx;
+    ss << txPrev.nTime << prevout.hash << prevout.n << ssUniqueID << nTimeTx;
     hashProofOfStake = Hash(ss.begin(), ss.end());
 //    LogPrintf("bnStakeModifierV2: %s: modifier:%d nTimeBlockFrom:%d nTimeTx:%d hash:%s\n", __func__, bnStakeModifierV2, prevout.hash, prevout.n, hashProofOfStake.GetHex());
 
