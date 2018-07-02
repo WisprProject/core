2fe00ee8652bcc9f893bb08154e3c3976f71ff15
Thijs <thetcr@live.nl>
2018-07-02 22:03:48 +0200

    Vtx[0] timestamp equals that of block time

--- src/miner.cpp
+++ src/miner.cpp
@@ -147,7 +147,8 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
         if (nSearchTime >= nLastCoinStakeSearchTime) {
             unsigned int nTxNewTime = 0;
             if (pwallet->CreateCoinStake(*pwallet, pblock->nBits, nSearchTime - nLastCoinStakeSearchTime, txCoinStake, nTxNewTime)) {
-                pblock->nTime = nTxNewTime;
+                vtx[0].nTime = nTime = txCoinStake.nTime;
+//                pblock->nTime = nTxNewTime;
                 pblock->vtx[0].vout[0].SetEmpty();
                 pblock->vtx.push_back(CTransaction(txCoinStake));
                 fStakeFound = true;
