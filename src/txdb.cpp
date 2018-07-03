4f7f0885de1369ddac0139d0ac47a7ebacaf5448
Thijs <thetcr@live.nl>
2018-07-03 18:42:10 +0200

    Use old hash

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -261,6 +261,7 @@ bool CBlockTreeDB::LoadBlockIndexGuts()
                 pindexNew->prevoutStake = diskindex.prevoutStake;
                 pindexNew->nStakeTime = diskindex.nStakeTime;
                 pindexNew->hashProofOfStake = diskindex.hashProofOfStake;
+                pindexNew->hashProof = diskindex.hashProof;
 
                 if (pindexNew->IsProofOfWork()) {
                     if (!CheckProofOfWork(pindexNew->GetBlockHeader().GetPoWHash(), pindexNew->nBits))
