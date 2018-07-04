03891ec120be6f75f6248e3cacc170771950f19b
Thijs <thetcr@live.nl>
2018-07-04 21:42:07 +0200

    Add new checkpoints

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -59,7 +59,9 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
                 (75000, uint256("06f162fe22851c400c1532a6d49d7894640ea0aa292fad5f02f348480da6b20d")) // Block 75.000
                 (100000, uint256("ed8cccfb51c901af271892966160686177a05f101bd3fd517d5b82274a8f6611")) // Block 100.000
                 (125000, uint256("76d5412ec389433de6cd22345209c859b4c18b6d8f8893df479c9f7520d19901")) // Block 125.000
-                (150000, uint256("a7e0dfdc9c3197e9e763e858aafa9553c0235c0e328371a5f8c5ba0b6e44919d")); // Block 150.000
+                (150000, uint256("a7e0dfdc9c3197e9e763e858aafa9553c0235c0e328371a5f8c5ba0b6e44919d")) // Block 150.000
+                (200000, uint256("385e915b52f0ad669b91005ab7ddb22356b6a220e8b98cbcf2c8aca5c5dd3b03")) // Block 200.000
+                (250000, uint256("40ee22bd8b2cc23f83e16d19a53aa8591617772f9722c56b86d16163b2a10416")); // Block 250.000
 static const Checkpoints::CCheckpointData data = {
         &mapCheckpoints,
         1523519952, // * UNIX timestamp of last checkpoint block (Done)
@@ -73,7 +75,7 @@ static Checkpoints::MapCheckpoints mapCheckpointsTestnet =
         boost::assign::map_list_of(0, uint256("0x001"));
 static const Checkpoints::CCheckpointData dataTestnet = {
         &mapCheckpointsTestnet,
-        1528378203,
+        1512932225,
         0,
         450};
 
