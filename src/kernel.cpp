422e43d91454f63b5e9da9a82120a8da1ff87363
Thijs <thetcr@live.nl>
2018-07-05 21:06:05 +0200

    Logging

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -415,7 +415,7 @@ bool Stake(CStakeInput* stakeInput, unsigned int nBits, unsigned int nTimeBlockF
             if (!CheckStake(ssUniqueID, stakeInput->GetValue(), nStakeModifier, bnTargetPerCoinDay, nTimeBlockFrom, nTryTime, hashProofOfStake))
                 continue;
         }else{
-            if (!CheckStake(txPrev, txin.prevout, nTryTime, hashProofOfStake, nValueIn, chainActive.Tip(true), nBits, true))
+            if (!CheckStake(txPrev, txin.prevout, nTryTime, hashProofOfStake, nValueIn, chainActive.Tip(true), nBits))
             {
                 continue;
             }
@@ -423,7 +423,7 @@ bool Stake(CStakeInput* stakeInput, unsigned int nBits, unsigned int nTimeBlockF
         }
 
         fSuccess = true; // if we make it this far then we have successfully created a stake hash
-        //LogPrintf("%s: hashproof=%s\n", __func__, hashProofOfStake.GetHex());
+        LogPrintf("%s: hashproof=%s\n", __func__, hashProofOfStake.GetHex());
         nTimeTx = nTryTime;
         break;
     }
