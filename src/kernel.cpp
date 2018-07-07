f2462de6761c6fa657a8d96b42e5f17c7ea77939
Thijs <thetcr@live.nl>
2018-07-07 05:39:59 +0200

    Start cleanup

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -375,7 +375,7 @@ bool CheckStake(const CTransaction& txPrev, const COutPoint& prevout,
         LogPrintf("%s :  bnTarget * bnCoinDayWeight=%s \n", function, ((bnTarget.getuint256()).ToString()));
     }
     // Now check if proof-of-stake hash meets target protocol
-    if (hashProofOfStake > bnTarget.getuint256())
+    if (hashProofOfStake > bnTarget)
         return false;
 
     return true;
