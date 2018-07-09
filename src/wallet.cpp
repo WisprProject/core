12b9be3e9ece5d15e68d0c15b34130552e13a647
Thijs <thetcr@live.nl>
2018-07-09 13:35:16 +0200

    Whoops

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1961,7 +1961,7 @@ void CWallet::AvailableCoins(vector<COutput>& vCoins, bool fOnlyConfirmed, const
                     found = !IsDenominatedAmount(pcoin->vout[i].nValue);
                     if (found && fMasterNode) found = pcoin->vout[i].nValue != 125000 * COIN; // do not use Hot MN funds
                 } else if (nCoinType == ONLY_10000) {
-                    found = pcoin->vout[i].nValue == 125000 * COIN;
+                    found = pcoin->vout[i].nValue == 10000 * COIN;
                 } else {
                     found = true;
                 }
