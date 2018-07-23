a5bf23d18f6645b6075cb4e376798db1aedcad35
Thijs <thetcr@live.nl>
2018-07-24 01:21:34 +0200

    cleanup

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -517,7 +517,7 @@ bool CheckProofOfStake(const CBlock block, uint256& hashProofOfStake, std::uniqu
     } else {
 //        uint256 bnTargetPerC = bnTargetPerCoinDay.GetCompact();
         if (!CheckStakeV1(txPrev.nTime, txin.prevout, tx.nTime, hashProofOfStake, nValueIn, chainActive.Tip(), block.nBits, true)) {
-            DeleteBlockAndInputs(block.vtx[1]);
+            DeleteTip();
             LogPrintf("Stake(): Delete Tip\n");
             return error("CheckProofOfStake() : INFO: old bnStakeModifierV2 check kernel failed on coinstake %s, hashProof=%s \n",
                   tx.GetHash().ToString(), hashProofOfStake.ToString());
@@ -561,7 +561,7 @@ void DeleteTip()
     CValidationState state;
     {
         LOCK(cs_main);
-        DisconnectBlocksAndReprocess(1);
+        ReprocessBlocks(3);
     }
 }
 void DeleteBlockAndInputs(CTransaction txLock)
