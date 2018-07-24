ad0f64d711f92c07d61e11d917200e9608deda77
Thijs <thetcr@live.nl>
2018-07-24 12:04:48 +0200

    cleanup

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -533,7 +533,7 @@ bool CheckCoinStakeTimestamp(int64_t nTimeBlock, int64_t nTimeTx)
 {
     // v0.3 protocol
     int64_t mask = (nTimeTx & STAKE_TIMESTAMP_MASK);
-    LogPrintf("%u \n", mask);
+    LogPrintf("CheckCoinStakeTimestamp(): %u \n", mask);
     return (nTimeBlock == nTimeTx) && ((nTimeTx & STAKE_TIMESTAMP_MASK) == 0);
 }
 
